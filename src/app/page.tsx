b9277365cc4ae9764ae3490775e177af55420a81
Mahdi Sarrafzadeh <mahdisarrafz9@gmail.com>
2024-01-19 02:50:23 +0330

    refactor: update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
           setActiveKey={setActiveKey}
           activeKey={activeKey}
         />
-        {!status.includes("succeeded") ? (
+        {status !== "succeeded" ? (
           <Loading />
         ) : products && products.length === 0 ? (
           <NotFound text={string.notFound} />
