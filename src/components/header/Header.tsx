0a9adc22515662e4a728458a095262f5775de113
Mahdi Sarrafzadeh <mahdisarrafz9@gmail.com>
2024-01-19 11:33:47 +0330

    feat: add link for logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,22 @@
 import { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import { header as string } from "@/utils/string";
 
 const Header: FC = () => {
   return (
     <header className="bg-white px-[60px] flex justify-between items-center h-[72px] shrink-0">
-      <Image
-        src="/logo.svg"
-        alt="Testio Logo"
-        width={100}
-        height={24}
-        priority
-        className="object-cover w-auto h-auto"
-      />
+      <Link href={"/"}>
+        <Image
+          src="/logo.svg"
+          alt="Testio Logo"
+          width={100}
+          height={24}
+          priority
+          className="object-cover w-auto h-auto"
+        />
+      </Link>
       <section className="flex flex-row px-[20px] py-2 border-solid border-[1px] max-sm:rounded-full max-sm:px-2 rounded-[50px] cursor-pointer ">
         <Image
           src="/account_circle.svg"
